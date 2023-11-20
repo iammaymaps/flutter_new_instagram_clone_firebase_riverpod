45a574cd858c370228a05151a01f413a6c03c64f
iammaymaps <atom5896@gmail.com>
2023-11-20 23:06:21 +0600

    app bar

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomeScreen extends StatefulWidget {
   HomeScreen({super.key});
@@ -20,8 +21,34 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("HomeScreen"),
-      ),
+          toolbarHeight: 80.0, // Set the desired height
+          backgroundColor: Colors.black,
+          actions: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 5),
+              child: IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.favorite_border_rounded,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 5),
+              child: IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.send_rounded,
+                ),
+              ),
+            ),
+          ],
+          title: SvgPicture.network(
+            "https://www.vectorlogo.zone/logos/instagram/instagram-wordmark.svg",
+            color: Colors.white,
+            height: 45,
+            width: 45,
+          )),
       body: Column(children: [Text("I am alright ")]),
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: Colors.black,
@@ -29,6 +56,8 @@ class _HomeScreenState extends State<HomeScreen> {
         unselectedItemColor: Colors.grey,
         showSelectedLabels: false,
         showUnselectedLabels: false,
+        selectedFontSize: 23.0, // Set the font size for the selected item
+        unselectedFontSize: 23.0, // Set the font size for unselected items
         items: [
           BottomNavigationBarItem(
             icon: Icon(Icons.home_rounded),
