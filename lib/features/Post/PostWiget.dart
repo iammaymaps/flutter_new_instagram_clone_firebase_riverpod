31f0c69ab71eaa45e176e8b2bff33ba303afe6f1
iammaymaps <atom5896@gmail.com>
2023-11-26 12:11:08 +0600

    post Widget 1

--- lib/features/Post/PostWiget.dart
+++ lib/features/Post/PostWiget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:readmore/readmore.dart';
 
 class PostWidget extends StatelessWidget {
   const PostWidget({super.key});
@@ -7,7 +8,7 @@ class PostWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
-      height: 550,
+      height: 620,
       color: Colors.white,
       child: Padding(
         padding: const EdgeInsets.symmetric(vertical: 25),
@@ -39,20 +40,108 @@ class PostWidget extends StatelessWidget {
                     ],
                   ),
                   IconButton(
-                      onPressed: () {}, icon: Icon(Icons.more_vert_outlined))
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.more_vert_outlined,
+                        size: 25,
+                      ))
                 ],
               ),
             ),
-            ClipRect(
-              child: Container(
-                child: Image.asset(
-                  "assets/images/Post image test.png",
-                  fit: BoxFit.fitHeight,
-                  width: double.infinity,
-                  height: double.infinity,
+            const SizedBox(
+              height: 10,
+            ),
+            Image.asset(
+              "assets/images/Post image test.png",
+              fit: BoxFit.cover,
+              width: double.infinity,
+              height: 350,
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.favorite_border_rounded,
+                        )),
+                    const SizedBox(
+                      width: 5,
+                    ),
+                    IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.chat_bubble_outline_rounded,
+                        )),
+                    const SizedBox(
+                      width: 5,
+                    ),
+                    IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.send_rounded,
+                        )),
+                  ],
                 ),
+                IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.bookmark_border_rounded,
+                    )),
+              ],
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Row(
+                children: [
+                  Text(
+                    "241 likes",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17),
+                  ),
+                ],
               ),
-            )
+            ),
+            const SizedBox(
+              height: 6,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 5),
+              child: ReadMoreText(
+                'Flutter is Google’s mobile UI open source framework to build high-quality native (super fast) interfaces for iOS and Android apps with the unified codebase.',
+                trimLines: 1,
+                colorClickableText: Colors.pink,
+                trimMode: TrimMode.Line,
+                trimCollapsedText: 'Show more',
+                trimExpandedText: 'Show less',
+                moreStyle:
+                    TextStyle(fontSize: 14, fontWeight: FontWeight.normal),
+              ),
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            TextButton(
+              onPressed: () {},
+              child: Row(
+                children: [
+                  Text(
+                    "View all 2 comments",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15,
+                        color: Colors.black),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
